b8b4933718e56a0ac7cc3b1146f8bd170bc1ca6e
kenta-crv <okuyamakenta@gmail.com>
2021-07-09 16:29:44 +0900

    fix

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,16 +1,16 @@
-class TrialMailer < ActionMailer::Base
+class ContactMailer < ActionMailer::Base
   default from: "info@okurite.jp"
-  def received_email(trial)
-    @trial = trial
+  def received_email(contact)
+    @contact = contact
     mail to: "info@okurite.jp"
     mail(subject: 'Okuriteよりお問い合わせがありました') do |format|
       format.text
     end
   end
 
-  def send_email(trial)
-    @trial = trial
-    mail to: trial.email
+  def send_email(contact)
+    @contact = contact
+    mail to: contact.email
     mail(subject: 'Okuriteにお問い合わせ頂きありがとうございます｜株式会社Ri-Plus') do |format|
       format.text
     end
